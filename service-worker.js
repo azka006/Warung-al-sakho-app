247e53f8aab2680060d651478de9eb278a6af13b
azka006 <faiz.n.alhuda@gmail.com>
2025-10-09 14:27:24 +0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,7 +7,19 @@ const urlsToCache = [
   'icon-512.png',
   'https://cdn.jsdelivr.net/npm/html5-qrcode@2.3.8/html5-qrcode.min.js',
   'https://cdn.jsdelivr.net/npm/chart.js@4.4.4/dist/chart.umd.min.js',
-  'toko.json'
+  'toko.json',
+  '<link rel="preconnect" href="https://fonts.googleapis.com">',
+  '<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>',
+  '<link href="https://fonts.googleapis.com/css2?family=Bitcount+Prop+Single+Ink:wght@100..900&family=Caprasimo&display=swap" rel="stylesheet">',
+  'script.js',
+  'style.css',
+  'theme-estetik.css',
+  'theme-elegan.css',
+  'theme-alam.css',
+  'theme-angkasa.css',
+  'theme-snow.css',
+  'theme-sunset.css',
+  'theme-spidermanNoir.css'
 ];
 
 // Install service worker
